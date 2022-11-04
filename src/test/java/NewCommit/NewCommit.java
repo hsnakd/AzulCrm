f62e2953d3bf9ef84039df69c40a917800b587d9
hsnakd <hsnakd10@gmail.com>
2022-11-04 16:58:06 +0300

    new commit 3

--- src/test/java/NewCommit/NewCommit.java
+++ src/test/java/NewCommit/NewCommit.java
@@ -2,6 +2,7 @@ package NewCommit;
 
 public class NewCommit {
     public static void main(String[] args) {
-        System.out.println("new commit");
+        System.out.println("new commit2");
+        System.out.println("new commit3");
     }
 }
